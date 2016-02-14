c98a593c37978345369d72fd37537f2a7fac89ab
Jennifer Li <ldnjennifer@hotmail.com>
2016-02-13 19:33:04 -0800

    change background music

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,13 @@
 class Post < ActiveRecord::Base
   belongs_to :category
-belongs_to :user  
+belongs_to :user
 has_many :comments, :dependent => :destroy
 
   validates :title, presence: true, uniqueness:true
   self.per_page = 10
 
   def self.search(str)
-    where("title ILIKE ? OR body ILIKE ?", "%#{str}%", "%#{str}%")
+    where("title ILIKE ? OR body ILIKE? ", "%#{str}%", "%#{str}%")
   end
 
 end
