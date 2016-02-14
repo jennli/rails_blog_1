f31a003ea36f1d40b770916ad7ba258cac393f7e
Jennifer Li <ldnjennifer@hotmail.com>
2016-02-13 21:09:21 -0800

    fix bug for category requirement

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,8 @@ belongs_to :user
 has_many :comments, :dependent => :destroy
 
   validates :title, presence: true, uniqueness:true
+  validates :category_id, presence: true
+
   self.per_page = 10
 
   def self.search(str)
