7a0bb55b1e985540fba0674aa14719b7ae5a9569
Jennifer Li <ldnjennifer@hotmail.com>
2016-02-13 16:54:32 -0800

    more bug fixes for showing current user for blog

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,7 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new post_params
+    @post.user = current_user
     if   @post.save
       # flash[:notice] = "Question created successfully"
       redirect_to post_path(@post), notice: "post created successfully"
