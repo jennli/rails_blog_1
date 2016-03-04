b391e5217f6fa748efcbee8ac52fe2489bcc5c2d
Jennifer Li <ldnjennifer@hotmail.com>
2016-03-04 14:09:32 -0800

    changing mail setup to point to sendgrid address

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,9 +1,8 @@
 ActionMailer::Base.smtp_settings = {
-  address:              "smtp.gmail.com",
+ :address => 'smtp.sendgrid.net',
   port:                 "587",
   enable_starttls_auto: true,
   authentication:       :plain,
   user_name:            ENV["email_user_name"],
-  domain:                "ldn-rails-blog.herokuapp.com",
   password:             ENV["email_password"]
 }
